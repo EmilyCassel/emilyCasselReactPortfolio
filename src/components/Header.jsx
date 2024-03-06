0521f631bd6d86325c375b653a797921a0db9076
emily <emilycassel77@gmail.com>
2024-03-06 15:05:19 -0500

    css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,16 @@
 
 
-function Header (){
-    return(
+function Header() {
+    return (
         <div className="header">
             <h1 className="myName">Emily Cassel</h1>
-            <div className="homePageLinks">
-                <nav>
-                    <h2><a className="homeLink"href="/">Home</a></h2>
-                    <h2><a className="portfolioLink" href="/Portfolio">Portfolio</a></h2>
-                    <h2><a className="contactLink" href="/contact">Contact</a></h2>
-                </nav>
-            </div>
-            
+            <nav className="homePageLinks">
+                <h2><a className="mainLink" href="/">Home</a></h2>
+                <h2><a className="mainLink" href="/Portfolio">Portfolio</a></h2>
+                <h2><a className="mainLink" href="/contact">Contact</a></h2>
+            </nav>
+
+
         </div>
     )
 }
