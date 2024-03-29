d40a2984c17037309f2d5e6952df6ca1682eac7c
emily <emilycassel77@gmail.com>
2024-03-29 18:06:02 -0400

    aboutmepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,14 +8,15 @@ function Home() {
             <img className="aboutMeImage" src={emilyCassel} alt="myPhoto" />
             <h2><a className="linkedInLink" href="https://www.linkedin.com/in/emily-cassel-5a2980245/">LinkedIn</a></h2>
             <h2><a className="resumeLink" href="/Resume">Resume</a></h2>
+            <h2><a className="aboutMeLink" href="/AboutMe">About Me</a></h2>
          </div>
          <div className="aboutMeInformation">
             <h1 className="aboutMeHeader">About Me</h1>
-            <p className="aboutMeParagraph">
-               As a Full-Stack Web Developer, my journey has been one of resilience, exploration, and discovering my true passion.</p>
+            <div className="aboutMeParagraph">
+               As a Full-Stack Web Developer, my journey has been one of resilience, exploration, and discovering my true passion.</div>
 
-            <p className="aboutMeParagraph">
-               My academic journey commenced at 18, where I initially pursued nursing at Ball State University. However, life threw me a curveball at 20 when I faced health challenges. Despite the obstacles, I persevered and made the decision to transfer to IvyTech Community College in 2013. Over the next four years, amidst battling health issues, I realized that nursing wasn't my destined path. Thus, I transitioned my focus to Exercise Physiology.</p>
+            <div className="aboutMeParagraph">
+               My academic journey commenced at 18, where I initially pursued nursing at Ball State University. However, life threw me a curveball at 20 when I faced health challenges. Despite the obstacles, I persevered and made the decision to transfer to IvyTech Community College in 2013. Over the next four years, amidst battling health issues, I realized that nursing wasn't my destined path. Thus, I transitioned my focus to Exercise Physiology.</div>
             <p className="aboutMeParagraph">
                In 2021, I proudly graduated from the University of Lynchburg with a degree in Exercise Physiology. However, the onset of COVID-19 limited opportunities, prompting me to embark on a three-year journey of travel. This period allowed me to fulfill many dreams that were once hindered by illness, while also delving into the world of logistics in my spare time. I passed the Import/Export Logistics Exam in October 2023.
             </p>
