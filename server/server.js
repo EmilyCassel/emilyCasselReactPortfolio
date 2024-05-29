b794fce8340e267ca90f9b1d4a5b45af1da90e37
emily <emilycassel77@gmail.com>
2024-05-29 17:20:23 -0400

    server folder and client folder completed

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('../client/dist')) //builds out routes automatically
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../client/dist/index.html'))
+    })
+}
+
+const PORT = process.env.PORT || 3014;
+
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
+
