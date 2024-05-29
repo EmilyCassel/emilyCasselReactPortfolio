047c588c5e03e22fbc402c6354dd9f44925b09b6
emily <emilycassel77@gmail.com>
2024-05-29 18:10:58 -0400

    fixed server.js

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(clientPath)) //builds out routes automatically
 
     app.get('*', (req, res) => {
-        res.sendFile(path.join(clientPath, '../client/dist/index.html'))
+        res.sendFile(path.join(clientPath, 'index.html'))
     })
 }
 
