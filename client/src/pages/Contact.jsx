92af7ded80c2f0be406f809b75312be9cab31367
emily <emilycassel77@gmail.com>
2024-07-19 19:39:02 -0400

    Update Contact.jsx to send messages to emilycassel77@gmail.com

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -32,7 +32,7 @@ function Contact() {
       setTimeout(() => {
         setShowSuccessMessage(false);
       }, 3000);
-      setFormData({ name: '', email: '', message: '' }); // Clear form after submission
+      setFormData({ name: '', email: '', message: '' }); 
     })
     .catch((error) => {
       alert('Failed to send message');
