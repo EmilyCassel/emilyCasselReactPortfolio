2d8de2562b903f15baf7408f2a699b6ed4520488
emily <emilycassel77@gmail.com>
2024-07-21 13:35:28 -0400

    home

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -3,3 +3,4 @@ export { default as Contact } from './Contact';
 export { default as Portfolio } from './Portfolio';
 export { default as Resume } from './Resume';
 export { default as AboutMe } from './AboutMe';
+import '../index.scss'; 
